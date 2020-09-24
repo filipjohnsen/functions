c300de04d6a2ebe155caef9a19fef946c429de6f
Filip Johnsen <filipjohnsen97@gmail.com>
2020-09-24 18:20:57 +0200

    now?

--- src/go.js
+++ src/go.js
@@ -1,38 +1,14 @@
-const express = require("express");
-const serverless = require("serverless-http");
+const express = require("express")
+const serverless = require("serverless-http")
 
-const app = express();
-const router = express.Router();
+const app = express()
 
-router.get("/:id/:subid", (req, res) => {
-  const id = req.params.id
-  const subid = req.params.subid
+const router = express.Router()
 
-  redirectToProvider(id, subid, res)
-});
+router.get("/", (req, res) => {
+    return res.send("hello world")
+})
 
-app.use(`/.netlify/functions/go`, router);
+app.use("/go", router)
 
-function redirectToProvider(id, subid, res) {
-  switch (id) {
-    case "expressvpn":
-      res.redirect("https://www.expressvpn.com/fi/6ysrp?offer=3monthsfree&a_fid=55101&chan=" + subid)
-      break;
-    case "nordvpn":
-      res.redirect("https://go.nordvpn.net/aff_c?offer_id=280&aff_id=8456&url_id=1718&aff_sub=" + subid)
-      break;
-    case "surfshark":
-      res.redirect("https://get.surfshark.net/aff_c?offer_id=20&aff_id=1389&aff_sub=" + subid)
-      break;
-    case "ultravpn":
-      res.redirect("http://track.ultravpn.com/5c6e86713dfba/click/" + subid)
-      break;
-    case "cyberghostvpn":
-      res.redirect("https://www.cyberghostvpn.com/offer/paras7vpn_mwq6ww5bh?source=paras7vpn.fi")
-    default:
-      break;
-  }
-}
-
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app)
